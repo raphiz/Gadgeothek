fa6b44858ebec9d6f21e6fbfcbac106838ce9eb2
Jonas Matter <jonas.matter@hsr.ch>
2016-10-06 16:27:45 +0200

    initial activity and fragment setup

--- app/src/main/java/ch/hsr/gadgeothek/ui/GadgetDetailActivity.java
+++ app/src/main/java/ch/hsr/gadgeothek/ui/GadgetDetailActivity.java
@@ -1,13 +1,13 @@
-package ch.hsr.gadgeothek;
+package ch.hsr.gadgeothek.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GadgetDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gadget_detail);
     }
 }
