fc62235c51779df3101c43a011b39d8db0389eed
Jonas Matter <jonas.matter@hsr.ch>
2016-10-06 16:37:35 +0200

    add missing import statements

--- app/src/main/java/ch/hsr/gadgeothek/ui/SignupActivity.java
+++ app/src/main/java/ch/hsr/gadgeothek/ui/SignupActivity.java
@@ -1,7 +1,9 @@
 package ch.hsr.gadgeothek.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ch.hsr.gadgeothek.R;
 
 public class SignupActivity extends AppCompatActivity {
 
