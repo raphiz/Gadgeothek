b52b1752eb7c9c52c4ac95780419966a430e3f22
Robin Suter <suter.robin@gmail.com>
2016-10-24 16:42:10 +0200

    cleanup unused code

--- app/src/main/java/ch/hsr/gadgeothek/ui/GadgetDetailCallback.java
+++
@@ -1,9 +0,0 @@
-package ch.hsr.gadgeothek.ui;
-
-import ch.hsr.gadgeothek.domain.Gadget;
-import ch.hsr.gadgeothek.domain.Reservation;
-
-public interface GadgetDetailCallback {
-    void onReserve(Gadget gadget);
-    void onReserveDelete(Reservation reservation);
-}
